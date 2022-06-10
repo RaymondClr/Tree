d27d0c61e276c306041749220021a131403d193a
Raymond Yan <RaymondClr@outlook.com>
2022-06-10 22:18:24 +0800

    Update nativeControl.jsx

--- test/nativeControl.jsx
+++ test/nativeControl.jsx
@@ -1,42 +1,21 @@
 //@include "../tree.jsx"
 
-// var elements = Tree.parse({
-//     button: [],
-//     checkbox: [],
-//     dropdownlist: [],
-//     edittext: [],
-//     flashplayer: [],
-//     group: [],
-//     iconbutton: [],
-//     image: [],
-//     listbox: [],
-//     panel: [],
-//     progressbar: [],
-//     radiobutton: [],
-//     scrollbar: [],
-//     slider: [],
-//     statictext: [],
-//     tabbedpanel: [],
-//     treeview: [],
-// });
-
-var window = new Window('palette');
-var panel = window.add('panel');
-var editText = panel.add('edittext');
-var button = panel.add('button');
-window.show();
-
-var window = new Window('palette { \
-    panel: Panel { \
-        editText: EditText { }, \
-        button: Button { } \
-    }, \
-}');
-window.show();
-
-var window = Tree.parse({
-    panel: {
-        editText: [],
-        button: [],
-    },
+var elements = Tree.parse({
+    button: [],
+    checkbox: [],
+    dropdownlist: [],
+    edittext: [],
+    flashplayer: [],
+    group: [],
+    iconbutton: [],
+    image: [],
+    listbox: [],
+    panel: [],
+    progressbar: [],
+    radiobutton: [],
+    scrollbar: [],
+    slider: [],
+    statictext: [],
+    tabbedpanel: [],
+    treeview: [],
 });
